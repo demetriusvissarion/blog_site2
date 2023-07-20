2855450bd6c7738cb5bfaeb017fbc3317766643b
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-20 15:29:35 +0100

    enqueue styles and add wp_head()

--- front-page.php
+++ front-page.php
@@ -8,15 +8,19 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="Blog Site Template">
-	<meta name="author" content="https://youtube.com/FollowAndrew">
+	<meta name="author" content="#">
 	<link rel="shortcut icon" href="/wp-content/themes/blog_site2/assets/images/logo.png">
 
 	<!-- FontAwesome CSS-->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"> -->
 	<!-- Bootstrap CSS-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
 	<!-- Theme CSS -->
-	<link rel="stylesheet" href="/wp-content/themes/blog_site2/assets/css/style.css">
+	<!-- <link rel="stylesheet" href="/wp-content/themes/blog_site2/assets/css/style.css"> -->
+
+	<?php
+	wp_head();
+	?>
 
 </head>
 
@@ -124,7 +128,7 @@
 		</article>
 		<footer class="footer text-center py-2 theme-bg-dark">
 
-			<p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p>
+			<p class="copyright"><a href="#">Copyright 2023</a></p>
 
 		</footer>
 
@@ -135,7 +139,9 @@
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-	<script src="/wp-content/themes/blog_site2/assets/js/main.js"></script>
+	<!-- <script src="/wp-content/themes/blog_site2/assets/js/main.js"></script> -->
+	<?php wp_head(); ?>
+
 
 </body>
 
--- functions.php
+++ functions.php
@@ -1,9 +1,18 @@
 <?php
 
-function blog_site2_register_styles() {
-	wp_enqueue_style('blog_site2-bootstrap', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
+function blog_site2_register_styles()
+{
+	$version = wp_get_theme()->get('Version');
+	wp_enqueue_style('blog_site2-style', get_template_directory_uri() . "/assets/css/style.css", array('blog_site2-bootstrap'), $version, 'all');
+	wp_enqueue_style('blog_site2-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', array(), '4.4.1', 'all');
+	wp_enqueue_style('blog_site2-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 };
 
 add_action('wp_enqueue_scripts', 'blog_site2_register_styles');
 
-?>
+
+function blog_site2_register_scripts()
+{
+};
+
+add_action('wp_enqueue_scripts', 'blog_site2_register_scripts');
