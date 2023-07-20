a2a38c9b117ae98550ae781887b3d5e05f4ea636
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-20 16:45:50 +0100

    split in components & reconnect in front-page.php

--- front-page.php
+++ front-page.php
@@ -4,7 +4,14 @@ get_header();
 
 <article class="content px-3 py-5 p-md-5">
 
-	Home Page content
+	<?php
+	if (have_posts()) {
+		while (have_posts()) {
+			the_post();
+			the_content();
+		}
+	}
+	?>
 
 </article>
 
