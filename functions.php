a35875b720c96e96aaa1a4630657c4d10a385731
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-21 12:14:42 +0100

    single post template file

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ function blog_site2_theme_support()
 	add_theme_support('title-tag');
 
 	add_theme_support('custom-logo');
+	add_theme_support('post-thumbnails');
 };
 
 add_action('after_setup_theme', 'blog_site2_theme_support');
